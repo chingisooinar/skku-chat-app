c22877df37b37703b4b938d7ba7a75ef6de85fe5
chingis <chingisoinar@gmail.com>
2022-05-11 14:38:12 +0900

    react structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import io from "socket.io-client";
+
+let SOCKET;
+const URL = 'localhost:3001/'
 
 function App() {
   const [authorized, setAuthorized] = useState(false);
   const [room, setRoom] = useState("");
   const [studentID, setStudentID] = useState("");
-  
+
   const [message, setMessage] = useState("");
   const [chatHistory, setChatHistory] = useState([]);
+  // this is a hook to be called every time URL changes (const)
+  useEffect(() => {
+    SOCKET = io(URL);
+    console.log(SOCKET.connected)
+  }, [URL]);
+  // this is a hook to be called on every page render
+  useEffect(() => {
+    // socket: on recieved messages add them into chatHistory using setChatHistory
 
-  
+  });
+
+  function isNum(val){
+    return !isNaN(val)
+  }
   const connectToRoom = () => {
-    setAuthorized(true);
-    //socket 
+    if (studentID.length === 10 && isNum(studentID)){
+      setAuthorized(true);
+      //socket: join room here
+
+      //
+    } else {
+      alert('Please type in a valid student ID')
+    }
   }
 
   const sendMessage = () => {
+
     let messageObj = {
       room: room,
       content: {
@@ -23,14 +46,20 @@ function App() {
         message: message,
       },
     };
-    // socket
-    console.log(message);
+    // socket send message here
+
+    //
     setChatHistory([...chatHistory, messageObj.content]);
+    // clear
+    var messageInput = document.getElementById("messageInput");
+    messageInput.value = "";
     setMessage("");
+
   }
   return (
     <div className="App">
-      {! authorized ? 
+      {! authorized ?
+      //This is HTML for authorization
       <div className="container">
         <div className="login-form">
             <input
@@ -40,26 +69,29 @@ function App() {
                 setStudentID(e.target.value);
               }}
             />
-            <input
-              type="text"
-              placeholder="Room"
-              onChange={(e) => {
+            <select id = "RoomList" placeholder="Room" onChange={(e) => {
                 setRoom(e.target.value);
-              }}
-            />
+              }} >
+          <option> w3schools </option>
+          <option> Javatpoint </option>
+          <option> tutorialspoint </option>
+          <option> geeksforgeeks </option>
+          </select>
+
           </div>
           <button onClick={connectToRoom}>Enter Chat</button>
 
-      
+
       </div>
       :
+      //This is HTML after authorization
       <div className="chat-container">
       <div className="messages">
         {chatHistory.map((val, key) => {
           return (
             <div
               className="message-container"
-              id={val.sender == studentID ? "you" : "other"}
+              id={val.sender == studentID ? "mymessage" : "outsidemessage"}
             >
               <div className="message-box">
                 {val.sender}: {val.message}
@@ -72,16 +104,23 @@ function App() {
       <div className="messageInputs">
         <input
           type="text"
+          id="messageInput"
           placeholder="Message"
           onChange={(e) => {
             setMessage(e.target.value);
+
           }}
         />
-        <button onClick={sendMessage}>Send</button>
+        {message.length > 0 ?
+          <button onClick={sendMessage}>Send</button>
+          :
+          <button onClick={sendMessage} disabled={true}>Send</button>
+        }
+
       </div>
     </div>
       }
-      
+
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const socket = require("socket.io");
 app.use(cors())
 
 app.listen("3001", () => {
     console.log('Server is Running!');
 });
+ // socket
